4d5be93cccc19a68fca325334344633981f181d5
Ronaldo <ronaldo@digitall.co.nz>
2017-06-07 21:23:30 +1200

    Remove config files. Adjust main with test data

--- main.go
+++ main.go
@@ -2,23 +2,26 @@ package main
 
 import (
 	"fmt"
-	"github.com/rmsj/stock/db/models"
 	"database/sql"
-	"github.com/vattle/sqlboiler/queries/qm"
+	
+	"github.com/rmsj/stock/db/models"
+	_ "github.com/go-sql-driver/mysql"
 )
+
+//go:generate sqlboiler --basedir "./db/" --tinyint-as-bool mysql
 
 func main() {
 	
 	// Open handle to database like normal
-	db, err := sql.Open("mysql", "stock_dba:secret@tcp(192.168.10.10:3306)/stock?charset=utf8")
+	conn, err := sql.Open("mysql", "stock_dba:secret@tcp(192.168.10.10:3306)/stock?charset=utf8")
 	if err != nil {
 		fmt.Errorf("error: %v", err)
 	}
 	
 	// SELECT COUNT(*) FROM pilots;
-	count, err := models.Countries(db, qm.Limit(5)).Count()
+	count, err := models.Countries(conn).Count()
 	if err != nil {
-		fmt.Errorf("error: %v", db)
+		fmt.Errorf("Error: %v", err)
 	}
 	fmt.Println(count)
 }
